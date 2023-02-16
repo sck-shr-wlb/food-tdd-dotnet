d44b7de41e2495aac1d298f05b6423557f64766d
pachara.mee <pachara.mee@cpf.co.th>
2023-02-16 08:23:49 +0700

    [refector] remove if condition 6 and 7. change if conditon equal 5 to if condition geater than equal 5.

--- RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals.cs
@@ -7,17 +7,17 @@ public class RomanNumerals
 
         while (number != 0)
         {
-            if (number == 7)
-            {
-                result += "VII";
-                number -= 7;
-            }
-            else if (number == 6)
-            {
-                result += "VI";
-                number -= 6;
-            }
-            else if (number == 5)
+            // if (number == 7)
+            // {
+            //     result += "VII";
+            //     number -= 7;
+            // }
+            // else if (number == 6)
+            // {
+            //     result += "VI";
+            //     number -= 6;
+            // }
+             if (number >= 5)
             {
                 result += "V";
                 number -= 5;
